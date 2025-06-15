75919b0dc6d018c6ee10b04f3b8958d7b259dc96
Jayanth Pasupulati <75296693+jaypasnagasai@users.noreply.github.com>
2025-06-15 16:45:17 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="biolookup",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["pandas"],
+    entry_points={
+        "console_scripts": [
+            "biolookup=biolookup.run:main",
+        ]
+    },
+)
